b9ae220494c39187e139f239ec0cc17c4d9990c3
Vincent Garcia <vgarcia.account@pm.me>
2022-04-13 15:37:41 +0200

    Bump mbo_version

--- ps_mbo.php
+++ ps_mbo.php
@@ -139,14 +139,14 @@ class ps_mbo extends Module
     public function __construct()
     {
         $this->name = 'ps_mbo';
-        $this->version = '2.0.2';
+        $this->version = '2.1.0';
         $this->author = 'PrestaShop';
         $this->tab = 'administration';
         $this->module_key = '6cad5414354fbef755c7df4ef1ab74eb';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
             'min' => '1.7.5.0',
-            'max' => _PS_VERSION_,
+            'max' => '8.0.0',
         ];
 
         parent::__construct();
