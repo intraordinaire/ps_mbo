382ee88eee6a5021cee2684d5a8a4a9b3e5e9c9d
Vincent Garcia <vgarcia.account@pm.me>
2022-04-14 10:13:54 +0200

    ⚗️ Test autoupgrade file

--- autoupgrade.php
+++ autoupgrade.php
@@ -1,15 +1,43 @@
 <?php
 
+//Fetch the true updated module here
 $moduleUpdateUrl = 'https://api-addons.prestashop.com/?method=module&version=8.0&id_module=4181';
+
+// Test purpose
 $moduleUpdateUrl = _PS_MODULE_DIR_ . 'test.zip';
-$archiveFilepath = _PS_MODULE_DIR_ . 'module_update.zip';
+
+$archiveFilepath = _PS_MODULE_DIR_ . 'module_mbo_update.zip';
 file_put_contents($archiveFilepath, file_get_contents($moduleUpdateUrl));
 
 if (file_exists($archiveFilepath)) {
     $zip = new ZipArchive();
     if ($zip->open($archiveFilepath) === true) {
+        deleteFolder(_PS_MODULE_DIR_ . 'ps_mbo', true);
         $zip->extractTo(_PS_MODULE_DIR_);
         $zip->close();
         unlink($archiveFilepath);
+    }
+}
+
+function deleteFolder($folder, $rootFolder = false)
+{
+    $filesToKeep = [
+        'autoupgrade.php',
+    ];
+
+    $dir = opendir($folder);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            $full = $folder . '/' . $file;
+            if (is_dir($full)) {
+                deleteFolder($full);
+            } elseif (!in_array($file, $filesToKeep)) {
+                unlink($full);
+            }
+        }
+    }
+    closedir($dir);
+    if (!$rootFolder) {
+        rmdir($folder);
     }
 }
